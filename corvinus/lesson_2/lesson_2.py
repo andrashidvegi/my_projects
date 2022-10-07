7d3046c3f615d219b875bf9c5b71fe90d02c0d2a
Andras Hidvegi <andras.hidvegi@incepteam.com>
2022-10-07 16:17:10 +0200

    lesson 2 first ideas

---
+++ corvinus/lesson_2/lesson_2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df_prices = pd.read_csv('input/prices.csv')
+df_prices_adjusted = pd.read_csv('input/prices-split-adjusted.csv')
+df_securities = pd.read_csv('input/securities.csv')
+df_fundamentals = pd.read_csv('input/fundamentals.csv')
+
+# df_pivot_prices =
+
+
+# Processing: filter to needed cols, dates to datetime -> lambda
+
+# pandas pivot to have it with symbols as columns
+
+# To teach before:
